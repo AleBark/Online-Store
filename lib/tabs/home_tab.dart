673631588cdb67af40a636714d29a39033191196
Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>
2019-09-01 14:02:53 -0300

    rendering images on home_tab

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,5 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   @override
@@ -7,8 +9,8 @@ class HomeTab extends StatelessWidget {
     Widget _buildBodyBack() => Container(
           decoration: BoxDecoration(
               gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 211, 118, 130),
-            Color.fromARGB(255, 253, 181, 168)
+            Color.fromARGB(255, 47, 51, 62),
+            Color.fromARGB(255, 92, 99, 119)
           ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
         );
 
@@ -32,8 +34,8 @@ class HomeTab extends StatelessWidget {
                   .collection("home")
                   .orderBy("pos")
                   .getDocuments(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if(!snapshot.hasData){
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
                   return SliverToBoxAdapter(
                     child: Container(
                       height: 200.0,
@@ -41,17 +43,23 @@ class HomeTab extends StatelessWidget {
                       child: CircularProgressIndicator(
                         valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
                       ),
-
                     ),
                   );
-                }else{
-                  print(snapshot.data.documents.length);
-                  return SliverToBoxAdapter(
-                    child: Container(
-                      height: 200.0,
-                      alignment: Alignment.center,
-                      child: Container()
-                    ),
+                } else {
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data.documents.map((doc) {
+                      return StaggeredTile.count(doc.data["x"], doc.data["y"]);
+                    }).toList(),
+                    children: snapshot.data.documents.map((doc) {
+                      return FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: doc.data["image"],
+                        fit: BoxFit.cover,
+                      );
+                    }).toList(),
                   );
                 }
               },
