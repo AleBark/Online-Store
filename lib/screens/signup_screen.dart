f84d5f4180d4e7f66c9fe8a090d19ecaeda07fe5
Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>
2019-11-09 00:31:29 -0300

    returning null on form validator

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,89 +1,145 @@
 import 'package:flutter/material.dart';
+import 'package:online_store_app/models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-class SignupScreen extends StatelessWidget {
+class SignupScreen extends StatefulWidget {
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _addressController = TextEditingController();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sign up"),
-        centerTitle: true,
-        actions: <Widget>[],
-      ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16.0),
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(hintText: "Name"),
-              validator: (text) {
-                if (text.isEmpty || text.length < 3) {
-                  return "Invalid name";
-                } else {
-                  return '';
-                }
-              },
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              decoration: InputDecoration(hintText: "Address"),
-              validator: (text) {
-                if (text.isEmpty || text.length < 3) {
-                  return "Invalid address";
-                } else {
-                  return '';
-                }
-              },
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              decoration: InputDecoration(hintText: "E-mail"),
-              validator: (text) {
-                if (text.isEmpty || !text.contains("@")) {
-                  return "Invalid e-mail";
-                } else {
-                  return '';
-                }
-              },
-            ),
-            SizedBox(height: 16.0),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "Password",
-              ),
-              validator: (text) {
-                if (text.isEmpty || text.length < 6) {
-                  return "Invalid password";
-                } else {
-                  return '';
-                }
-              },
-              obscureText: true,
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            SizedBox(
-              height: 44.0,
-              child: RaisedButton(
-                child: Text(
-                  "Signup",
-                  style: TextStyle(
-                    fontSize: 18.0,
-                  ),
-                ),
-                textColor: Colors.white,
-                color: Theme.of(context).primaryColor,
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {}
-                },
-              ),
-            )
-          ],
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text("Sign up"),
+          centerTitle: true,
+          actions: <Widget>[],
         ),
-      ),
-    );
+        body: ScopedModelDescendant<UserModel>(
+          builder: (BuildContext context, Widget child, model) {
+            if (model.isLoading) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return Form(
+              key: _formKey,
+              child: ListView(
+                padding: EdgeInsets.all(16.0),
+                children: <Widget>[
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(hintText: "Name"),
+                    validator: (text) {
+                      if (text.isEmpty) {
+                        return  "Invalid name";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: _addressController,
+                    decoration: InputDecoration(hintText: "Address"),
+                    validator: (text) {
+                      if (text.isEmpty) {
+                        return "Invalid address";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(hintText: "E-mail"),
+                    validator: (text) {
+                      if (text.isEmpty || !text.contains("@")) {
+                       return "Invalid e-mail";
+                      }
+
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                    ),
+                    validator: (text) {
+                      if (text.isEmpty) {
+                        return "Invalid password";
+                      }
+                      return null;
+                    },
+                    obscureText: true,
+                  ),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  SizedBox(
+                    height: 44.0,
+                    child: RaisedButton(
+                      child: Text(
+                        "Signup",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      textColor: Colors.white,
+                      color: Theme.of(context).primaryColor,
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          Map<String, dynamic> userData = {
+                            "name": _nameController.text,
+                            "email": _emailController.text,
+                            "address": _addressController.text
+                          };
+
+                          model.signUp(
+                              userData: userData,
+                              pass: _passwordController.text,
+                              onSuccess: _onSuccess,
+                              onFail: _onFail
+                          );
+                        }
+                      },
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+        ));
+  }
+
+  void _onSuccess() {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text("User created"),
+      backgroundColor: Theme.of(context).primaryColor,
+      duration: Duration(seconds: 2),
+    ));
+    Future.delayed(Duration(seconds: 2)).then((_) {
+      Navigator.of(context).pop();
+    });
+  }
+
+  void _onFail() {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text("User not created"),
+      backgroundColor: Colors.redAccent,
+      duration: Duration(seconds: 2),
+    ));
   }
 }
